1ca224f346a7833ab9fef85c2b6955ef75a8d081
Muhammad Mustafa Amir <mustafaamir398a@gmail.com>
2026-07-17 18:01:41 +0500

    Update higher_order_function_case3.dart

--- 3.Functions/higher_order_function_case3.dart
+++ 3.Functions/higher_order_function_case3.dart
@@ -1,1 +1,15 @@
+import 'dart:io';
 
+Function sum() {
+  int a = 5, b = 6, total = 0;
+  total = a + b;
+
+  return () {
+    return total;
+  };
+}
+
+void main() {
+  Function x = sum();
+  print(x()); // yahan call karna zaroori hai result dekhne ke liye
+}
