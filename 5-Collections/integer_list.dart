1f4983d686dd2d39ab8ae1cf41beb2048c509107
Muhammad Mustafa Amir <mustafaamir398a@gmail.com>
2026-08-03 00:18:08 +0500

    Implement for-in loop to print list elements
    Added a main function to demonstrate a for-in loop.

--- 5-Collections/integer_list.dart
+++ 5-Collections/integer_list.dart
@@ -1,1 +1,9 @@
-
+void main() {
+  List<int> l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  
+  // for-in loop version (Dart-idiomatic way)
+  print("Elements in list are: ");
+  for (int element in l1) {
+    print(element);
+  }
+}
